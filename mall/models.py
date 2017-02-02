8103edfed84ca66e9c6526aee4ccebfc0073532b
Ohiro Oluwatosin Akinkunmi <ohirotosin@gmail.com>
2017-02-02 17:44:31 +0100

    Second commit

--- mall/models.py
+++ mall/models.py
@@ -1,5 +1,42 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField('first name', max_length=30)
+    last_name = models.CharField('last name', max_length=30)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 # Create your models here.
@@ -8,7 +45,7 @@ class UserManager(BaseUserManager):
 
     def _create_user(self, email, first_name, last_name, password, **extra_fields):
         """
-        Creates and saves a User with the given username, email and password.
+        Creates and saves a User with the given email, firstname, lastmame, and password.
         """
         if not email:
             raise ValueError('The email must be set')
@@ -33,9 +70,6 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(email, first_name, last_name, password, **extra_fields)
-
-
-
 
 
 class Owner(models.Model):
