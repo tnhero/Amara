cb668eb535be23f438f674aff38e99934f384984
Ohiro Oluwatosin Akinkunmi <ohirotosin@gmail.com>
2017-02-03 14:29:32 +0100

    Now

--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View, generic
 
 # Create your views here.
+
+
+class IndexView (View):
+    template_name = 'mall/index.html'
